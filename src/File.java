831447c8f695baf1baa5397cf9072ddf8d5ea256
talshalom98 <talshalom@campus.technion.ac.il>
2021-06-21 11:19:55 +0300

    fcd

--- src/File.java
+++ src/File.java
@@ -1,8 +1,8 @@
 
 
 public class File extends StorageItem {
-    private String name;
-    private String ext;
+    private final String name;
+    private final String ext;
     private String content;
 
     public File(String name, String ext) {
