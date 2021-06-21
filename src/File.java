f54ed817688186ecddb8e39b07fd59a23f3b2e14
talshalom98 <talshalom@campus.technion.ac.il>
2021-06-21 11:02:52 +0300

    file

---
+++ src/File.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+
+public class File extends StorageItem {
+    private String name;
+    private String ext;
+    private String content;
+
+    public File(String name, String ext) {
+        this.name = name;
+        this.ext = ext;
+        this.content = "";
+
+    }
+
+    public String getName() {
+        String official_name = this.name + "." + this.ext;
+        return official_name;
+    }
+
+    public int getSize() {
+        return this.content.length();
+    }
+
+    public void addContent (String contentToAdd){
+        this.content = this.content + contentToAdd;
+    }
+
+    public void printContent(){
+        System.out.println(getName(), "Size: ", getSize(), "MB Created: ",super.date);
+        System.out.println(this.content);
+    }
+}
