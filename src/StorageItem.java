ef91508e97b26a34893f91e5599c25012f662a6f
tomer shigani <tomershigani@campus.technion.ac.il>
2021-06-21 15:59:17 +0300

    update

--- src/StorageItem.java
+++ src/StorageItem.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.sql.Timestamp;
+import java.lang.Math;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.*;
@@ -14,9 +15,16 @@ public abstract class StorageItem {
  private final long date;
 
 public StorageItem(String name){
+    Timestamp times1 = new Timestamp(2017,1,1,0,0,0,0);
+    Timestamp times2 = new Timestamp(2021,1,1,0,0,0,0);
+    long time1 = times1.getTime();
+    long time2 = times2.getTime();
+    Random rnd=Main.rnd;
+    long date1 = Math.abs(rnd.nextLong());
+    date1 = (date1 %(Math.abs(time1 - time2)))+ Math.min(time1,time2);
     this.name = name;
     this.size = size;
-    this.date = date;
+    this.date = date1;
 }
 
 public abstract int getSize();
