00dace7da8cec13f4d82948bfdbb6a4f872e6d3d
tomer shigani <tomershigani@campus.technion.ac.il>
2021-06-21 11:51:50 +0300

    StorageItem

--- src/StorageItem.java
+++ src/StorageItem.java
@@ -1,3 +1,28 @@
-public class StorageItem {
+import java.util.Random;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.*;
+
+
+
+
+public abstract class StorageItem {
+
+ private String name;
+ private double size;
+ private final long date;
+
+public StorageItem(String name,double size, long date){
+    this.name = name;
+    this.size = size;
+    this.date = date;
+}
+
+public abstract int getSize();
+
+public void printTree(SortingField field){
+
+}
 
 }
